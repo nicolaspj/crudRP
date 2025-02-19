19994a69c51b79b9ff0d0ce3422a32d4cc13d8ab
Nicolas C <cardozo.j.nicolas@gmail.com>
2025-02-19 14:14:56 -0300

    Agregado de carpeta Edicion admin/user/products

---
+++ src/HomeUserAdmin/Edicion/EdicionProducto.jsx
@@ -0,0 +1,13 @@
+import "../../App.css";
+import InfoProducts from "./InfoProducts";
+
+function HomeUsu() {
+  return (
+    <div className="parrafoInfoProducts">
+      <h1>Edicion de Productos</h1>
+      <InfoProducts />
+    </div>
+  );
+}
+
+export default HomeUsu;
---
+++ src/HomeUserAdmin/Edicion/EdicionUser.jsx
@@ -0,0 +1,10 @@
+import "../../App.css";
+
+function EdicionUser() {
+  return (
+    <div className="bodyEdicionUser">
+      <h1>Sitio en construccion</h1>
+    </div>
+  );
+}
+export default EdicionUser;
